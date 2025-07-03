6488a9c7a4a1ce662d7be38a134e16c1801db10c
Aarush_0312 <aarushkumarchoubey@gmail.com>
2025-07-03 13:06:47 +0000

    modified the code

--- app.py
+++ app.py
@@ -464,6 +464,6 @@ def delete_goal(goal_id):
 # Initialize database
 with app.app_context():
   db.create_all()
-
-if __name__=='__main__':
-  app.run(host='0.0.0.0',port=5000, debug=True)
+  
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080,debug=True) 
